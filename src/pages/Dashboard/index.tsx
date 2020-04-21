4d4bd956cd2df55139c90023dcb56dadd45ac53c
Luis Paulo Ruda <luispauloruda@gmail.com>
2020-04-21 09:20:25 -0300

    Estilizando Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,77 @@
 import React from 'react';
-import { Title } from './styles';
+import { FiChevronRight } from 'react-icons/fi';
+
+import logoImg from '../../assets/images/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github.</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github.</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars3.githubusercontent.com/u/35377905?s=460&u=13ce29ab7e21a38284b72412f6cbd0485ddd112b&v=4"
+            alt="Luis Paulo"
+          />
+
+          <div>
+            <strong>Unform</strong>
+            <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars3.githubusercontent.com/u/35377905?s=460&u=13ce29ab7e21a38284b72412f6cbd0485ddd112b&v=4"
+            alt="Luis Paulo"
+          />
+
+          <div>
+            <strong>Unform</strong>
+            <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars3.githubusercontent.com/u/35377905?s=460&u=13ce29ab7e21a38284b72412f6cbd0485ddd112b&v=4"
+            alt="Luis Paulo"
+          />
+
+          <div>
+            <strong>Unform</strong>
+            <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars3.githubusercontent.com/u/35377905?s=460&u=13ce29ab7e21a38284b72412f6cbd0485ddd112b&v=4"
+            alt="Luis Paulo"
+          />
+
+          <div>
+            <strong>Unform</strong>
+            <p>Easy peasy highly scalable ReactJS & React Native forms!</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
